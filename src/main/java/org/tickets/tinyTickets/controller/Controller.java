f0b6912be2318b5c93b7e0c92554dea3ad3b97e8
xpbll <xpbllll@mail.ru>
2019-05-20 21:23:00 +0300

    Default

--- src/main/java/org/tickets/tinyTickets/controller/Controller.java
+++ src/main/java/org/tickets/tinyTickets/controller/Controller.java
@@ -1,6 +1,8 @@
 package org.tickets.tinyTickets.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -9,8 +11,9 @@ import java.util.Map;
 public class Controller {
 
     @RequestMapping("/")
-    public String homePage(Map<String, Object> model){
-        model.put("some", "By Yura Yarmoshuk");
-        return "main";
+    public String homePage(@RequestParam(name = "text", required = false, defaultValue = "SOME TEXT") String name,
+                           Model model){
+        model.addAttribute("text", name);
+        return "index";
     }
 }
